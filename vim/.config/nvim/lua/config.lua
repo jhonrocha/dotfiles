90dbf23d84c698942ecf7bb90b725c7820f75aff
Jhon Rocha <jhmrocha@gmail.com>
2021-08-05 11:27:35 -0300

    Updating

--- vim/.config/nvim/lua/config.lua
+++ vim/.config/nvim/lua/config.lua
@@ -3,8 +3,8 @@
 require('lualine').setup {
   options = {
     theme = 'github',
-    section_separators = {"", ""},
-    component_separators = {"", ""},
+    -- section_separators = {"", ""},
+    -- component_separators = {"", ""},
   },
   sections = {
     lualine_a = {'mode'},
@@ -15,17 +15,18 @@ require('lualine').setup {
     lualine_z = {'location'}
   },
 }
+
 -- Theme
 require("github-theme").setup({
   themeStyle = "dark",
 })
 
--- Telescope
-require("plug-telescope")
 -- LSP
 local lspconfig = require'lspconfig'
+
 -- TS-JS
 lspconfig.tsserver.setup{}
+
 -- diagnosticls
 lspconfig.diagnosticls.setup{
   filetypes = {"javascript", "typescript"},
@@ -143,3 +144,59 @@ require'compe'.setup {
     vsnip = true;
   };
 }
+
+-- Telescope
+local actions = require('telescope.actions')
+local action_state = require('telescope.actions.state')
+require("telescope").setup {
+  defaults = {
+    vimgrep_arguments = {
+      'rg',
+      '--hidden',
+      '--color=never',
+      '--no-heading',
+      '--with-filename',
+      '--line-number',
+      '--glob=!.git',
+      '--column',
+      '--smart-case'
+    },
+    sorting_strategy = "ascending",
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        height = 0.6,
+        preview_width = 0.5,
+        prompt_position = "top",
+        preview_cutoff = 60,
+      },
+      vertical = {
+        mirror = false
+      }
+    },
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<c-j>"] = actions.move_selection_next,
+        ["<c-k>"] = actions.move_selection_previous,
+        ["<tab>"] = actions.add_selection + actions.move_selection_next,
+        ["<s-tab>"] = actions.remove_selection + actions.move_selection_previous
+      }
+    }
+  },
+  pickers = {
+    buffers = {
+      sort_lastused = true,
+      mappings = {
+        i = {
+          ["<c-d>"] = require("telescope.actions").delete_buffer,
+        },
+      }
+    }
+  }
+}
+function ff()
+  require('telescope.builtin').find_files {
+    find_command = {'fd','--type','f','--hidden','--follow','--exclude','.git'}
+  }
+end
