83543deb1f9967a993a36ceba9965068e3a678e2
Jhon Rocha <jhmrocha@gmail.com>
2021-08-18 20:24:19 -0300

    Updating

--- arch/sway-init.sh
+++ arch/sway-init.sh
@@ -3,7 +3,7 @@
 ##### INSTALL PKGS
 sudo pacman --noconfirm --needed -S \
     sway grim wl-clipboard slurp \
-    mako polkit-gnome \
+    swappy mako polkit-gnome \
     gtk3 gtk4 \
     qt5-wayland qt6-wayland
 
