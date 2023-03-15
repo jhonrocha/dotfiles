25fd4e8da48baa086b7c5d912269c3702d826f3d
Jhon Rocha <jhmrocha@gmail.com>
2023-03-15 10:34:34 -0300

    chore(catppuccin): adding leap integration to catppuccin

--- vim/.config/nvim/lua/plugins/ui.lua
+++ vim/.config/nvim/lua/plugins/ui.lua
@@ -15,6 +15,11 @@ return {
 		priority = 1000,
 		name = "catppuccin",
 		config = function()
+			require("catppuccin").setup({
+				integrations = {
+					leap = true,
+				},
+			})
 			vim.cmd.colorscheme("catppuccin-mocha")
 		end,
 	},
