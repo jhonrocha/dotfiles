ebfd22bd191de7e917f63ec1dad0071f923b726d
Jhon Rocha <jhmrocha@gmail.com>
2023-04-26 08:54:49 -0300

    fix(norg): updating to v3

--- vim/.config/nvim/lua/plugins/org.lua
+++ vim/.config/nvim/lua/plugins/org.lua
@@ -5,9 +5,9 @@ return {
 		opts = {
 			load = {
 				["core.defaults"] = {},
-				["core.norg.concealer"] = { config = { folds = false, icon_preset = "diamond" } },
+				["core.concealer"] = { config = { folds = false, icon_preset = "diamond" } },
 				["core.integrations.nvim-cmp"] = {},
-				["core.norg.completion"] = { config = { engine = "nvim-cmp" } },
+				["core.completion"] = { config = { engine = "nvim-cmp" } },
 			},
 		},
 		dependencies = { { "nvim-lua/plenary.nvim" } },
