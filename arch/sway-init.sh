1f74e5d1d76c3e286b22612bfa9d7afe23a34180
Jhon Rocha <jhmrocha@gmail.com>
2023-09-04 18:46:03 -0300

    Updating

--- arch/sway-init.sh
+++ arch/sway-init.sh
@@ -3,7 +3,7 @@
 ##### INSTALL PKGS
 sudo pacman --noconfirm --needed -S \
     sway swaybg grim wl-clipboard slurp \
-    swappy mako polkit-gnome \
+    swappy mako polkit-gnome gnome-keyring \
     gtk3 gtk4 qt5-wayland qt6-wayland \
-    seahorse \
+    seahorse xorg-xwayland acpilight \
     xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk
