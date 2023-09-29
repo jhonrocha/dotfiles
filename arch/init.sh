de15a21505b83b4c89a25026bfaff8f6557d4bcb
Jhon Rocha <jhmrocha@gmail.com>
2023-09-29 20:18:59 -0300

    Updating

--- arch/init.sh
+++ arch/init.sh
@@ -8,41 +8,25 @@ cd ~/dotfiles
 sudo pacman --noconfirm --needed -S - < pkg-all.txt
 
 #### Installing yay
-if ! command -v yay &> /dev/null
+if ! command -v paru &> /dev/null
 then
-    git clone https://aur.archlinux.org/yay.git ~/yay
-    cd ~/yay
-    makepkg -si
+  sudo pacman -S --needed base-devel
+  git clone https://aur.archlinux.org/paru.git
+  cd paru
+  makepkg -si
 fi
 
-### Install from AUR
-cd ~/dotfiles/arch
-yay -S --answerclean None --answerdiff None --answeredit None --noprovides --norebuild --needed - < pkg-aur.txt
+cd ~/dotfiles/
 
 #### STOW THE PACKAGES
 ./stowing.sh
-
-### Install i3
-./i3-init.sh
-
-#### Check for TPM
-[ ! -d "$HOME/.tmux/plugins/tpm" ] && git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 #### Setup Defaults
 xdg-mime default brave-broswer.desktop  x-scheme-handler/http
 xdg-mime default brave-browser.desktop x-scheme-handler/https
 xdg-settings set default-web-browser brave-browser.desktop
-
-# xdg-mime default vivaldi-stable.desktop  x-scheme-handler/http
-# xdg-mime default vivaldi-stable.desktop x-scheme-handler/https
-# xdg-settings set default-web-browser vivaldi-stable.desktop
 xdg-mime default org.pwmt.zathura.desktop application/pdf
 
 #### GIT Store
 git config --global credential.helper store
 git config --global core.excludesFile '~/.config/.git-ignore'
-
-##### Setting audio
-amixer sset Master unmute
-amixer sset Speaker unmute
-amixer sset Headphone unmute
