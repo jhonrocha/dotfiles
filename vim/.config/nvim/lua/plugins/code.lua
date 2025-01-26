642fe1899b1bc8034a6df3f3453966bd8bf19c8d
Jhon Rocha <jhmrocha@gmail.com>
2025-01-26 19:25:38 -0300

    improve virt text on vim ufo

--- vim/.config/nvim/lua/plugins/code.lua
+++ vim/.config/nvim/lua/plugins/code.lua
@@ -1,3 +1,29 @@
+local handler = function(virtText, lnum, endLnum, width, truncate)
+  local newVirtText = {}
+  local suffix = (" 󰁂 %d "):format(endLnum - lnum)
+  local sufWidth = vim.fn.strdisplaywidth(suffix)
+  local targetWidth = width - sufWidth
+  local curWidth = 0
+  for _, chunk in ipairs(virtText) do
+    local chunkText = chunk[1]
+    local chunkWidth = vim.fn.strdisplaywidth(chunkText)
+    if targetWidth > curWidth + chunkWidth then
+      table.insert(newVirtText, chunk)
+    else
+      chunkText = truncate(chunkText, targetWidth - curWidth)
+      local hlGroup = chunk[2]
+      table.insert(newVirtText, { chunkText, hlGroup })
+      chunkWidth = vim.fn.strdisplaywidth(chunkText)
+      -- str width returned from truncate() may less than 2nd argument, need padding
+      if curWidth + chunkWidth < targetWidth then suffix = suffix .. (" "):rep(targetWidth - curWidth - chunkWidth) end
+      break
+    end
+    curWidth = curWidth + chunkWidth
+  end
+  table.insert(newVirtText, { suffix, "MoreMsg" })
+  return newVirtText
+end
+
 local code = {
   {
     "ggandor/leap.nvim",
@@ -86,11 +112,10 @@ local code = {
     dependencies = "kevinhwang91/promise-async",
     opts = {
       close_fold_kinds_for_ft = {
-        default = { "imports", "comment" },
-        typescript = { "imports", "method_definition" },
         javascript = { "imports", "method_definition" },
       },
-      provider_selector = function(bufnr, filetype, buftype) return { "treesitter", "lsp" } end,
+      provider_selector = function(bufnr, filetype, buftype) return { "treesitter", "indent" } end,
+      fold_virt_text_handler = handler,
     },
   },
 }
