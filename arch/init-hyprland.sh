f49e228c8a666eb566eafef77a2454c530cd590b
Jhon Rocha <jhmrocha@gmail.com>
2025-01-31 11:03:15 -0300

    init-hyprland

--- arch/init-hyprland.sh
+++ arch/init-hyprland.sh
@@ -2,9 +2,9 @@
 
 ##### INSTALL PKGS
 sudo pacman --noconfirm --needed -S \
-    sway swaybg wl-clipboard \
+    hyprland hyprpolkitagent xdg-desktop-portal-hyprland\
     mako polkit-gnome gnome-keyring \
     gtk3 gtk4 qt5-wayland qt6-wayland \
     seahorse xorg-xwayland acpilight \
     xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk \
-    wofi
+    wofi wl-clipboard
