97f4a4d746c97efda655c67487d17f6f775dad14
Jhon Rocha <jhmrocha@gmail.com>
2025-01-31 10:58:35 -0300

    polkit setup

---
+++ arch/init-sway.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -v
+
+##### INSTALL PKGS
+sudo pacman --noconfirm --needed -S \
+    sway swaybg wl-clipboard \
+    mako polkit-gnome gnome-keyring \
+    gtk3 gtk4 qt5-wayland qt6-wayland \
+    seahorse xorg-xwayland acpilight \
+    xdg-desktop-portal xdg-desktop-portal-wlr xdg-desktop-portal-gtk \
+    wofi
