69e879033aeb4ea2f5b6642d8eb6a94705a6eac0
Jhon Rocha <jhmrocha@gmail.com>
2025-02-04 12:23:17 -0300

    fix wallpapers

--- hyprland/.config/dotbin/wallpaper.sh
+++ hyprland/.config/dotbin/wallpaper.sh
@@ -4,7 +4,7 @@ directory=~/.config/wallpapers
 monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
 
 if [ -d "$directory" ]; then
-    random_background=$(ls $directory/*.jpg | shuf -n 1)
+    random_background=$(ls $directory/*.{png,jpg,jpeg} | shuf -n 1)
     hyprctl hyprpaper unload all
     hyprctl hyprpaper preload $random_background
     hyprctl hyprpaper wallpaper "$monitor, $random_background"
