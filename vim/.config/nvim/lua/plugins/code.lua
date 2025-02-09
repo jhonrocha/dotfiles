22cd66e8aa6d2c6bf0531b4d29d67cff11e433c0
Jhon Rocha <jhmrocha@gmail.com>
2025-02-09 12:47:28 -0300

    update leetcode.nvim

--- vim/.config/nvim/lua/plugins/code.lua
+++ vim/.config/nvim/lua/plugins/code.lua
@@ -130,18 +130,26 @@ local code = {
     opts = {
       lang = "python3", -- configuration goes here
       picker = { provider = "telescope" },
+      injector = { ---@type table<lc.lang, lc.inject>
+        ["python"] = {
+          before = "from typing import List",
+        },
+        ["python3"] = {
+          before = "from typing import List",
+        },
+      },
     },
     keys = {
-      { "<leader>ll", "<Cmd>Leet<CR>",                                    desc = "leet" },
+      { "<leader>ll", "<Cmd>Leet<CR>",                                      desc = "leet" },
       { "<leader>le", "<Cmd>Leet random difficulty=easy status=todo<CR>",   desc = "easy" },
       { "<leader>lm", "<Cmd>Leet random difficulty=medium status=todo<CR>", desc = "medium" },
       { "<leader>lh", "<Cmd>Leet random difficulty=hard status=todo<CR>",   desc = "hard" },
-      { "<leader>lE", "<Cmd>Leet list difficulty=easy status=todo<CR>",   desc = "easy" },
-      { "<leader>lM", "<Cmd>Leet list difficulty=medium status=todo<CR>", desc = "medium" },
-      { "<leader>lH", "<Cmd>Leet list difficulty=hard status=todo<CR>",   desc = "hard" },
-      { "<leader>lr", "<Cmd>Leet run<CR>",                                desc = "run" },
-      { "<leader>ls", "<Cmd>Leet submit<CR>",                             desc = "submit" },
-      { "<leader>lo", "<Cmd>Leet open<CR>",                               desc = "open" },
+      { "<leader>lE", "<Cmd>Leet list difficulty=easy status=todo<CR>",     desc = "easy" },
+      { "<leader>lM", "<Cmd>Leet list difficulty=medium status=todo<CR>",   desc = "medium" },
+      { "<leader>lH", "<Cmd>Leet list difficulty=hard status=todo<CR>",     desc = "hard" },
+      { "<leader>lr", "<Cmd>Leet run<CR>",                                  desc = "run" },
+      { "<leader>ls", "<Cmd>Leet submit<CR>",                               desc = "submit" },
+      { "<leader>lo", "<Cmd>Leet open<CR>",                                 desc = "open" },
     },
   },
 }
