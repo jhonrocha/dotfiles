43ee2cdea9e50477170badac564cd1b9180bb6fa
Jhon Rocha <jhmrocha@gmail.com>
2025-03-13 16:39:50 -0300

    Updating

--- hyprland/.config/dotbin/wallpaper.sh
+++ hyprland/.config/dotbin/wallpaper.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-directory=~/.config/wallpapers
-monitor=`hyprctl monitors | grep Monitor | awk '{print $2}'`
+WALLPAPER_DIR="$HOME/.config/wallpapers/"
+CURRENT_WALL=$(hyprctl hyprpaper listloaded)
 
-if [ -d "$directory" ]; then
-    random_background=$(ls $directory/*.{png,jpg,jpeg} | shuf -n 1)
-    hyprctl hyprpaper unload all
-    hyprctl hyprpaper preload $random_background
-    hyprctl hyprpaper wallpaper "$monitor, $random_background"
-fi
+# Get a random wallpaper that is not the current one
+WALLPAPER=$(find "$WALLPAPER_DIR" -type f | shuf -n 1)
+
+# Apply the selected wallpaper
+hyprctl hyprpaper reload ,"$WALLPAPER"
