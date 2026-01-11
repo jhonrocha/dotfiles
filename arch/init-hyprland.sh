08634702157f8463750f7049687b2f24ab231f79
Jhon Rocha <jhmrocha@gmail.com>
2026-01-11 16:18:49 -0300

    updating for new hyprland install

--- arch/init-hyprland.sh
+++ arch/init-hyprland.sh
@@ -6,8 +6,20 @@ sudo pacman --noconfirm --needed -S \
     xdg-desktop-portal-hyprland \
     mako polkit-gnome gnome-keyring \
     gtk3 gtk4 qt5-wayland qt6-wayland \
-    seahorse xorg-xwayland acpilight \
+    seahorse xorg-xwayland \
     xdg-desktop-portal xdg-desktop-portal-gtk \
-    wofi wl-clipboard hyprlock hyprpicker
+    wofi wl-clipboard hyprlock hyprpicker \
+    waybar  swww  ghostty  nemo
 
-# paru hyprshot
+# HYPRLAND RELATED
+paru -S --needed hyprshot vicinae-bin
+
+# Basic
+sudo pacman --noconfirm --needed -S \
+  nvim tmux stow zsh ripgrep fzf eza \
+  ttf-iosevka-nerd ttf-jetbrains-mono-nerd noto-fonts-emoji 
+
+
+# Fancy
+sudo pacman --noconfirm --needed -S \
+  lazygit
