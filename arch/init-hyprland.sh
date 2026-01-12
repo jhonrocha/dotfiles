a6973fe81e448367933bb8cd1d6f912085d9d036
Jhon Rocha <jhmrocha@gmail.com>
2026-01-12 11:45:47 -0300

    Updating

--- arch/init-hyprland.sh
+++ arch/init-hyprland.sh
@@ -18,7 +18,8 @@ paru -S --needed hyprshot vicinae-bin
 # Basic
 sudo pacman --noconfirm --needed -S \
   git gh nvim tmux stow zsh ripgrep fzf eza \
-  ttf-iosevka-nerd ttf-jetbrains-mono-nerd noto-fonts-emoji 
+  ttf-iosevka-nerd ttf-jetbrains-mono-nerd noto-fonts-emoji \
+  tree-sitter tree-sitter-cli terminus-font
 
 # Fancy
 sudo pacman --noconfirm --needed -S \
