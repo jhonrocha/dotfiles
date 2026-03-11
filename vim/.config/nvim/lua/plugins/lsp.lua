4ca840b5a1b106e17a2962f242b2cc9b943c2331
Jhon Rocha <jhmrocha@gmail.com>
2026-03-11 10:55:56 -0300

    update gh dash

--- vim/.config/nvim/lua/plugins/lsp.lua
+++ vim/.config/nvim/lua/plugins/lsp.lua
@@ -44,13 +44,14 @@ local lsp = {
 				-- Conform will run multiple formatters sequentially
 				-- python = { "isort", "black", lsp_format = "last" },
 				-- You can customize some of the format options for the filetype (:help conform.format)
-				rust = { "rustfmt", lsp_format = "fallback" },
 				-- Conform will run the first available formatter
+				go = { "gofmt", lsp_format = "fallback" },
 				json = { "prettier", lsp_format = "never" },
 				javascript = { "prettier", stop_after_first = false, lsp_format = "never" },
 				javascriptreact = { "prettier", stop_after_first = false, lsp_format = "never" },
 				typescript = { "prettier", stop_after_first = false, lsp_format = "never" },
 				typescriptreact = { "prettier", stop_after_first = false, lsp_format = "never" },
+				rust = { "rustfmt", lsp_format = "fallback" },
 				sh = { "shfmt", lsp_format = "first" },
 				sql = { "sqfluff", lsp_format = "never" },
 			},
